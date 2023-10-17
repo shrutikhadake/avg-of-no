c062427eabc5555ba1c25606e6e5631b8da8a53c
shrutikhadake <148204864+shrutikhadake@users.noreply.github.com>
2023-10-17 15:34:03 +0530

    Create using while loop.cpp

---
+++ using while loop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int i=1,num,avg,sum=0;
+ while(1<=5)
+{
+ cout<<"Enter the number:"
+ cin>>num;
+ sum=sum+num;
+ i++;
+ avg=sum/5;
+ cout<<"The average is:"<<avg;
+ return 0;
+}
